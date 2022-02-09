a240df9aa4dca3a760340a1b70b23f693c4b7104
crackededed <90209774+crackededed@users.noreply.github.com>
2022-02-09 16:17:41 +0200

    channel page

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/channel/ChannelPagerFragment.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/channel/ChannelPagerFragment.kt
@@ -25,6 +25,14 @@ import com.github.andreyasadchy.xtra.ui.settings.SettingsActivity
 import com.github.andreyasadchy.xtra.util.*
 import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.fragment_channel.*
+import kotlinx.android.synthetic.main.fragment_channel.appBar
+import kotlinx.android.synthetic.main.fragment_channel.collapsingToolbar
+import kotlinx.android.synthetic.main.fragment_channel.follow
+import kotlinx.android.synthetic.main.fragment_channel.menu
+import kotlinx.android.synthetic.main.fragment_channel.search
+import kotlinx.android.synthetic.main.fragment_channel.toolbar
+import kotlinx.android.synthetic.main.fragment_channel.watchLive
+import kotlinx.android.synthetic.main.fragment_channel_old.*
 import kotlinx.android.synthetic.main.fragment_media_pager.*
 
 
@@ -45,7 +53,7 @@ class ChannelPagerFragment : MediaPagerFragment(), FollowFragment, Scrollable {
     private val viewModel by viewModels<ChannelPagerViewModel> { viewModelFactory }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_channel, container, false)
+        return inflater.inflate(if (requireContext().prefs().getString(C.USERNAME, "") != "") R.layout.fragment_channel else R.layout.fragment_channel_old, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -56,8 +64,25 @@ class ChannelPagerFragment : MediaPagerFragment(), FollowFragment, Scrollable {
         if (activity.isInLandscapeOrientation) {
             appBar.setExpanded(false, false)
         }
-        collapsingToolbar.title = requireArguments().getString(C.CHANNEL_DISPLAYNAME)
-        logo.loadImage(this, requireArguments().getString(C.CHANNEL_PROFILEIMAGE), circle = true)
+        if (requireContext().prefs().getString(C.USERNAME, "") != "") {
+            requireArguments().getString(C.CHANNEL_DISPLAYNAME).let {
+                if (it != null) {
+                    userLayout.visible()
+                    userName.visible()
+                    userName.text = it
+                }
+            }
+            requireArguments().getString(C.CHANNEL_PROFILEIMAGE).let {
+                if (it != null) {
+                    userLayout.visible()
+                    userImage.visible()
+                    userImage.loadImage(this, it, circle = true)
+                }
+            }
+        } else {
+            collapsingToolbar.title = requireArguments().getString(C.CHANNEL_DISPLAYNAME)
+            logo.loadImage(this, requireArguments().getString(C.CHANNEL_PROFILEIMAGE), circle = true)
+        }
         toolbar.apply {
             navigationIcon = Utils.getNavigationIcon(activity)
             setNavigationOnClickListener { activity.popFragment() }
@@ -111,50 +136,107 @@ class ChannelPagerFragment : MediaPagerFragment(), FollowFragment, Scrollable {
 
     override fun initialize() {
         val activity = requireActivity() as MainActivity
-        collapsingToolbar.expandedTitleMarginBottom = activity.convertDpToPixels(50.5f)
-        if (requireContext().prefs().getBoolean(C.API_USEHELIX, true) && requireContext().prefs().getString(C.USERNAME, "") != "") {
+        if (requireContext().prefs().getString(C.USERNAME, "") != "") {
             viewModel.loadStream(useHelix = true, clientId = requireContext().prefs().getString(C.HELIX_CLIENT_ID, ""), token = requireContext().prefs().getString(C.TOKEN, ""), channelId = requireArguments().getString(C.CHANNEL_ID), channelLogin = requireArguments().getString(C.CHANNEL_LOGIN), channelName = requireArguments().getString(C.CHANNEL_DISPLAYNAME), profileImageURL = requireArguments().getString(C.CHANNEL_PROFILEIMAGE))
-        } else {
-            viewModel.loadStream(useHelix = false, clientId = requireContext().prefs().getString(C.GQL_CLIENT_ID, ""), channelId = requireArguments().getString(C.CHANNEL_ID), channelLogin = requireArguments().getString(C.CHANNEL_LOGIN),channelName = requireArguments().getString(C.CHANNEL_DISPLAYNAME), profileImageURL = requireArguments().getString(C.CHANNEL_PROFILEIMAGE))
-        }
-        viewModel.stream.observe(viewLifecycleOwner) { stream ->
-            if (stream?.type?.lowercase() == "rerun") {
-                watchLive.text = getString(R.string.watch_rerun)
-                watchLive.setOnClickListener { activity.startStream(stream) }
-            } else {
-                if (stream?.viewer_count != null) {
-                    watchLive.text = getString(R.string.watch_live)
+            viewModel.stream.observe(viewLifecycleOwner) { stream ->
+                if (stream?.type?.lowercase() == "rerun") {
+                    watchLive.text = getString(R.string.watch_rerun)
                     watchLive.setOnClickListener { activity.startStream(stream) }
                 } else {
-                    watchLive.setOnClickListener { activity.startStream(Stream(user_id = requireArguments().getString(C.CHANNEL_ID), user_login = requireArguments().getString(C.CHANNEL_LOGIN), user_name = requireArguments().getString(C.CHANNEL_DISPLAYNAME), profileImageURL = requireArguments().getString(C.CHANNEL_PROFILEIMAGE))) }
+                    if (stream?.viewer_count != null) {
+                        watchLive.text = getString(R.string.watch_live)
+                        watchLive.setOnClickListener { activity.startStream(stream) }
+                    } else {
+                        watchLive.setOnClickListener { activity.startStream(Stream(user_id = requireArguments().getString(C.CHANNEL_ID), user_login = requireArguments().getString(C.CHANNEL_LOGIN), user_name = requireArguments().getString(C.CHANNEL_DISPLAYNAME), profileImageURL = requireArguments().getString(C.CHANNEL_PROFILEIMAGE))) }
+                    }
+                }
+                stream?.channelLogo.let {
+                    if (it != null) {
+                        userLayout.visible()
+                        userImage.visible()
+                        userImage.loadImage(this, it, circle = true)
+                        bundle.putString(C.CHANNEL_PROFILEIMAGE, it)
+                        arguments = bundle
+                    }
+                }
+                stream?.user_name.let {
+                    if (it != null && it != requireArguments().getString(C.CHANNEL_DISPLAYNAME)) {
+                        userLayout.visible()
+                        userName.visible()
+                        userName.text = it
+                        bundle.putString(C.CHANNEL_DISPLAYNAME, it)
+                        arguments = bundle
+                    }
+                }
+                stream?.user_login.let {
+                    if (it != null && it != requireArguments().getString(C.CHANNEL_LOGIN)) {
+                        bundle.putString(C.CHANNEL_LOGIN, it)
+                        arguments = bundle
+                    }
+                }
+                if (stream?.title != null) {
+                    streamLayout.visible()
+                    title.visible()
+                    title.text = stream.title.trim()
+                }
+                if (stream?.game_name != null) {
+                    streamLayout.visible()
+                    gameName.visible()
+                    gameName.text = stream.game_name
+                    if (stream.game_id != null) {
+                        gameName.setOnClickListener { activity.openGame(stream.game_id, stream.game_name) }
+                    }
+                }
+                if (stream?.viewer_count != null) {
+                    streamLayout.visible()
+                    viewers.visible()
+                    viewers.text = TwitchApiHelper.formatViewersCount(requireContext(), stream.viewer_count!!, requireContext().prefs().getBoolean(C.UI_TRUNCATEVIEWCOUNT, false))
+                }
+                if (requireContext().prefs().getBoolean(C.UI_UPTIME, true)) {
+                    if (stream?.started_at != null) {
+                        TwitchApiHelper.getUptime(requireContext(), stream.started_at).let {
+                            if (it != null)  {
+                                streamLayout.visible()
+                                uptime.visible()
+                                uptime.text = it
+                            }
+                        }
+                    }
+                }
+                if (requireArguments().getBoolean(C.CHANNEL_UPDATELOCAL) && stream != null) {
+                    viewModel.updateLocalUser(requireContext(), stream)
                 }
             }
-            stream?.channelLogo.let {
-                if (it != null) {
-                    logo.loadImage(this, it, circle = true)
-                    bundle.putString(C.CHANNEL_PROFILEIMAGE, it)
-                    arguments = bundle
+            viewModel.loadUser(useHelix = true, clientId = requireContext().prefs().getString(C.HELIX_CLIENT_ID, ""), token = requireContext().prefs().getString(C.TOKEN, ""), channelId = requireArguments().getString(C.CHANNEL_ID))
+            viewModel.user.observe(viewLifecycleOwner) { user ->
+                if (user != null) {
+                    if (user.created_at != null) {
+                        userCreated.visible()
+                        userCreated.text = requireContext().getString(R.string.created_at, TwitchApiHelper.formatTimeString(requireContext(), user.created_at))
+                    }
+                    if (user.followers_count != null) {
+                        userFollowers.visible()
+                        userFollowers.text = requireContext().getString(R.string.followers, TwitchApiHelper.formatCount(user.followers_count, requireContext().prefs().getBoolean(C.UI_TRUNCATEVIEWCOUNT, false)))
+                    }
+                    if (user.view_count != null) {
+                        userViews.visible()
+                        userViews.text = TwitchApiHelper.formatViewsCount(requireContext(), user.view_count, requireContext().prefs().getBoolean(C.UI_TRUNCATEVIEWCOUNT, false))
+                    }
+                    val broadcasterType = if (user.broadcaster_type != null) { TwitchApiHelper.getUserType(requireContext(), user.broadcaster_type) } else null
+                    val type = if (user.type != null) { TwitchApiHelper.getUserType(requireContext(), user.type) } else null
+                    val typeString = if (broadcasterType != null && type != null) "$broadcasterType, $type" else broadcasterType ?: type
+                    if (typeString != null) {
+                        userType.visible()
+                        userType.text = typeString
+                    }
                 }
             }
-            stream?.user_name.let {
-                if (it != null && it != requireArguments().getString(C.CHANNEL_DISPLAYNAME)) {
-                    collapsingToolbar.title = it
-                    bundle.putString(C.CHANNEL_DISPLAYNAME, it)
-                    arguments = bundle
-                }
+            if (requireContext().prefs().getBoolean(C.UI_FOLLOW, true)) {
+                initializeFollow(this, viewModel, follow, User.get(activity), context?.prefs()?.getString(C.HELIX_CLIENT_ID, ""))
             }
-            stream?.user_login.let {
-                if (it != null && it != requireArguments().getString(C.CHANNEL_LOGIN)) {
-                    bundle.putString(C.CHANNEL_LOGIN, it)
-                    arguments = bundle
-                }
-            }
-            if (requireArguments().getBoolean(C.CHANNEL_UPDATELOCAL) && stream != null) {
-                viewModel.updateLocalUser(requireContext(), stream)
-            }
-        }
-        if (requireContext().prefs().getBoolean(C.UI_FOLLOW, true)) {
-            initializeFollow(this, viewModel, follow, User.get(activity), context?.prefs()?.getString(C.HELIX_CLIENT_ID, ""))
+        } else {
+            collapsingToolbar.expandedTitleMarginBottom = activity.convertDpToPixels(50.5f)
+            watchLive.setOnClickListener { activity.startStream(Stream(user_id = requireArguments().getString(C.CHANNEL_ID), user_login = requireArguments().getString(C.CHANNEL_LOGIN), user_name = requireArguments().getString(C.CHANNEL_DISPLAYNAME), profileImageURL = requireArguments().getString(C.CHANNEL_PROFILEIMAGE))) }
         }
     }
 
