7392d7489e2731f82c02fb58f753f074a1de6241
crackededed <90209774+crackededed@users.noreply.github.com>
2022-06-21 14:32:14 +0300

    audio only button

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/common/ChatAdapter.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/common/ChatAdapter.kt
@@ -17,6 +17,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.text.getSpans
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.integration.webp.decoder.WebpDrawable
