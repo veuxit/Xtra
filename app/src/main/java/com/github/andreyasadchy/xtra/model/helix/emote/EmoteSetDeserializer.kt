ddcbb4800a4757755bba34f5b3b78d16ea2ffe51
Andrey Asadchy <samper475@gmail.com>
2022-10-15 09:39:07 +0200

    Fix animated Twitch emotes not animating in emotes menu

--- app/src/main/java/com/github/andreyasadchy/xtra/model/helix/emote/EmoteSetDeserializer.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/model/helix/emote/EmoteSetDeserializer.kt
@@ -7,29 +7,41 @@ import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import com.google.gson.JsonParseException
 import java.lang.reflect.Type
+import kotlin.math.min
 
 class EmoteSetDeserializer : JsonDeserializer<EmoteSetResponse> {
 
     @Throws(JsonParseException::class)
     override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): EmoteSetResponse {
         val data = mutableListOf<TwitchEmote>()
-        val dataJson = json.asJsonObject?.getAsJsonArray("data")
-        dataJson?.forEach { emote ->
-            emote?.asJsonObject?.let { obj ->
-                obj.get("name")?.asString?.let { name ->
-                    val urls = obj.getAsJsonObject("images")
-                    val url = urls.get(when (emoteQuality) {"4" -> ("url_4x") "3" -> ("url_4x") "2" -> ("url_2x") else -> ("url_1x")}).takeUnless { it?.isJsonNull == true }?.asString ?: urls.get("url_2x").takeUnless { it?.isJsonNull == true }?.asString ?: urls.get("url_1x").asString
-                    url?.let {
-                        val format = if (obj.getAsJsonArray("format")?.first()?.asString.equals("animated")) "image/gif" else null
-                        data.add(TwitchEmote(
-                            name = name,
-                            type = format,
-                            url = url,
-                            setId = obj.get("emote_set_id")?.asString,
-                            ownerId = obj.get("owner_id")?.asString
-                        ))
-                    }
-                }
+        val dataJson = json.asJsonObject.getAsJsonArray("data")
+        if (dataJson != null && dataJson.size() > 0) {
+            val template = json.asJsonObject.get("template").asString
+            val preferredScaleIndex = when (emoteQuality) {
+                "4", "3" -> 2
+                "2" -> 1
+                else -> 0
+            }
+            dataJson.forEach { emote ->
+                val obj = emote.asJsonObject
+                val id = obj.get("id").asString
+                val format = obj.getAsJsonArray("format").last().asString
+                val theme = obj.getAsJsonArray("theme_mode").last().asString
+                val scale = obj.getAsJsonArray("scale").let { it.get(min(preferredScaleIndex, it.size() - 1)) }.asString
+                val url = template
+                    .replaceFirst("{{id}}", id)
+                    .replaceFirst("{{format}}", format)
+                    .replaceFirst("{{theme_mode}}", theme)
+                    .replaceFirst("{{scale}}", scale)
+                data.add(
+                    TwitchEmote(
+                        name = obj.get("name").asString,
+                        type = if (format == "animated") "image/gif" else null,
+                        url = url,
+                        setId = obj.get("emote_set_id").asString,
+                        ownerId = obj.get("owner_id").asString
+                    )
+                )
             }
         }
         return EmoteSetResponse(data.sortedByDescending { it.setId })
