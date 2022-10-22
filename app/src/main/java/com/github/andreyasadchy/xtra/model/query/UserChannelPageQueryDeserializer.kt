5b81e656c9f7a80121411c4f4436c3dff2a12e57
crackededed <90209774+crackededed@users.noreply.github.com>
2022-10-22 14:38:15 +0300

    fix

--- app/src/main/java/com/github/andreyasadchy/xtra/model/query/UserChannelPageQueryDeserializer.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/model/query/UserChannelPageQueryDeserializer.kt
@@ -27,21 +27,25 @@ class UserChannelPageQueryDeserializer : JsonDeserializer<UserChannelPageQueryRe
                 thumbnail_url = obj.get("stream")?.takeIf { it.isJsonObject }?.asJsonObject?.get("previewImageURL")?.takeIf { !it.isJsonNull }?.asString,
                 profileImageURL = obj.get("profileImageURL")?.takeIf { !it.isJsonNull }?.asString,
                 channelUser = User(
-                    bannerImageURL = obj.get("bannerImageURL")?.takeIf { !it.isJsonNull }?.asString,
-                    view_count = obj.get("profileViewCount")?.takeIf { !it.isJsonNull }?.asInt,
-                    created_at = obj.get("createdAt")?.takeIf { !it.isJsonNull }?.asString,
-                    followers_count = obj.get("followers")?.takeIf { it.isJsonObject }?.asJsonObject?.get("totalCount")?.takeIf { !it.isJsonNull }?.asInt,
-                    broadcaster_type = when {
-                        obj.get("roles")?.takeIf { it.isJsonObject }?.asJsonObject?.get("isPartner")?.takeIf { !it.isJsonNull }?.asBoolean == true -> "partner"
-                        obj.get("roles")?.takeIf { it.isJsonObject }?.asJsonObject?.get("isAffiliate")?.takeIf { !it.isJsonNull }?.asBoolean == true -> "affiliate"
-                        else -> null
-                    },
+                    id = obj.get("id")?.takeIf { !it.isJsonNull }?.asString,
+                    login = obj.get("login")?.takeIf { !it.isJsonNull }?.asString,
+                    display_name = obj.get("displayName")?.takeIf { !it.isJsonNull }?.asString,
                     type = when {
                         obj.get("roles")?.takeIf { it.isJsonObject }?.asJsonObject?.get("isStaff")?.takeIf { !it.isJsonNull }?.asBoolean == true -> "staff"
                         obj.get("roles")?.takeIf { it.isJsonObject }?.asJsonObject?.get("isSiteAdmin")?.takeIf { !it.isJsonNull }?.asBoolean == true -> "admin"
                         obj.get("roles")?.takeIf { it.isJsonObject }?.asJsonObject?.get("isGlobalMod")?.takeIf { !it.isJsonNull }?.asBoolean == true -> "global_mod"
                         else -> null
                     },
+                    broadcaster_type = when {
+                        obj.get("roles")?.takeIf { it.isJsonObject }?.asJsonObject?.get("isPartner")?.takeIf { !it.isJsonNull }?.asBoolean == true -> "partner"
+                        obj.get("roles")?.takeIf { it.isJsonObject }?.asJsonObject?.get("isAffiliate")?.takeIf { !it.isJsonNull }?.asBoolean == true -> "affiliate"
+                        else -> null
+                    },
+                    profile_image_url = obj.get("profileImageURL")?.takeIf { !it.isJsonNull }?.asString,
+                    view_count = obj.get("profileViewCount")?.takeIf { !it.isJsonNull }?.asInt,
+                    created_at = obj.get("createdAt")?.takeIf { !it.isJsonNull }?.asString,
+                    followers_count = obj.get("followers")?.takeIf { it.isJsonObject }?.asJsonObject?.get("totalCount")?.takeIf { !it.isJsonNull }?.asInt,
+                    bannerImageURL = obj.get("bannerImageURL")?.takeIf { !it.isJsonNull }?.asString,
                 ),
                 lastBroadcast = obj.get("lastBroadcast")?.takeIf { !it.isJsonNull }?.asJsonObject?.get("startedAt")?.takeIf { !it.isJsonNull }?.asString,
             )
