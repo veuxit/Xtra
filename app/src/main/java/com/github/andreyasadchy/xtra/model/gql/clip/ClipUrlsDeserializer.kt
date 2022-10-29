af402caf803ea541ee3b262260e4594a3250fdd8
crackededed <90209774+crackededed@users.noreply.github.com>
2022-10-29 16:33:07 +0300

    playback token refactor

--- app/src/main/java/com/github/andreyasadchy/xtra/model/gql/clip/ClipUrlsDeserializer.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/model/gql/clip/ClipUrlsDeserializer.kt
@@ -1,5 +1,7 @@
 package com.github.andreyasadchy.xtra.model.gql.clip
 
+import android.net.Uri
+import com.github.andreyasadchy.xtra.model.gql.playlist.PlaybackAccessToken
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
@@ -11,14 +13,20 @@ class ClipUrlsDeserializer : JsonDeserializer<ClipUrlsResponse> {
     @Throws(JsonParseException::class)
     override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): ClipUrlsResponse {
         val data = mutableListOf<ClipUrlsResponse.ClipInfo>()
-        val dataJson = json.asJsonArray?.first()?.asJsonObject?.getAsJsonObject("data")?.getAsJsonObject("clip")?.getAsJsonArray("videoQualities")
-        dataJson?.forEach { item ->
+        val dataJson = json.asJsonObject?.getAsJsonObject("data")?.getAsJsonObject("clip")
+        val accessToken = dataJson?.get("playbackAccessToken")?.takeIf { it.isJsonObject }?.asJsonObject?.let { tokenJson ->
+            PlaybackAccessToken(
+                token = tokenJson.get("value")?.takeIf { !it.isJsonNull }?.asString,
+                signature = tokenJson.get("signature")?.takeIf { !it.isJsonNull }?.asString
+            )
+        }
+        dataJson?.getAsJsonArray("videoQualities")?.forEach { item ->
             item?.asJsonObject?.let { obj ->
                 obj.get("sourceURL")?.takeIf { !it.isJsonNull }?.asString?.let { url ->
                     data.add(ClipUrlsResponse.ClipInfo(
                         frameRate = obj.get("frameRate")?.takeIf { !it.isJsonNull }?.asInt,
                         quality = obj.get("quality")?.takeIf { !it.isJsonNull }?.asString,
-                        url = url.replace(Regex("https://[^/]+"),"https://clips-media-assets2.twitch.tv")
+                        url = "$url?sig=${Uri.encode(accessToken?.signature)}&token=${Uri.encode(accessToken?.token)}"
                     ))
                 }
             }
---
+++ app/src/main/java/com/github/andreyasadchy/xtra/model/gql/playlist/PlaybackAccessToken.kt
@@ -0,0 +1,3 @@
+package com.github.andreyasadchy.xtra.model.gql.playlist
+
+data class PlaybackAccessToken(val token: String?, val signature: String?)
