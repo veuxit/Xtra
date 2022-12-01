43cd9057db6409fe2a86ca392622c0f1866c1af2
crackededed <90209774+crackededed@users.noreply.github.com>
2022-12-01 15:59:44 +0200

    7tv v3

--- app/src/main/java/com/github/andreyasadchy/xtra/api/MiscApi.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/api/MiscApi.kt
@@ -23,11 +23,11 @@ interface MiscApi {
     @GET("https://recent-messages.robotty.de/api/v2/recent-messages/{channelLogin}")
     suspend fun getRecentMessages(@Path("channelLogin") channelLogin: String, @Query("limit") limit: String): Response<RecentMessagesResponse>
 
-    @GET("https://api.7tv.app/v2/emotes/global")
-    suspend fun getGlobalStvEmotes(): Response<StvEmotesResponse>
+    @GET("https://7tv.io/v3/emote-sets/global")
+    suspend fun getGlobalStvEmotes(): Response<StvGlobalResponse>
 
-    @GET("https://api.7tv.app/v2/users/{channelId}/emotes")
-    suspend fun getStvEmotes(@Path("channelId") channelId: String): Response<StvEmotesResponse>
+    @GET("https://7tv.io/v3/users/twitch/{channelId}")
+    suspend fun getStvEmotes(@Path("channelId") channelId: String): Response<StvChannelResponse>
 
     @GET("https://api.betterttv.net/3/cached/emotes/global")
     suspend fun getGlobalBttvEmotes(): Response<BttvGlobalResponse>
---
+++ app/src/main/java/com/github/andreyasadchy/xtra/model/chat/StvChannelDeserializer.kt
@@ -0,0 +1,44 @@
+package com.github.andreyasadchy.xtra.model.chat
+
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonParseException
+import java.lang.reflect.Type
+
+class StvChannelDeserializer : JsonDeserializer<StvChannelResponse> {
+
+    @Throws(JsonParseException::class)
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): StvChannelResponse {
+        val emotes = mutableListOf<StvEmote>()
+        val dataJson = json.asJsonObject.getAsJsonObject("emote_set").getAsJsonArray("emotes")
+        dataJson.forEach { emote ->
+            emote.asJsonObject.let { obj ->
+                obj.get("name").asString.let { name ->
+                    val urls = mutableListOf<String>()
+                    val host = obj.getAsJsonObject("data").getAsJsonObject("host")
+                    val template = host.get("url").asString
+                    host.getAsJsonArray("files").forEach { file ->
+                        file.asJsonObject.let { obj ->
+                            obj.get("name").asString.let { name ->
+                                if (!name.contains("avif", true)) {
+                                    urls.add("https:${template}/${name}")
+                                }
+                            }
+                        }
+                    }
+                    emotes.add(StvEmote(
+                        name = name,
+                        url1x = if (urls.size >= 1) urls[0] else null,
+                        url2x = if (urls.size >= 2) urls[1] else null,
+                        url3x = if (urls.size >= 3) urls[2] else null,
+                        url4x = if (urls.size >= 4) urls[3] else null,
+                        type = urls[0].substringAfterLast(".").let { "image/$it" },
+                        isZeroWidth = obj.get("flags")?.takeIf { !it.isJsonNull }?.asInt == 1
+                    ))
+                }
+            }
+        }
+        return StvChannelResponse(emotes)
+    }
+}
