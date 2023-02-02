37813dea472664aa7eb44e870eeb970a90d122b4
crackededed <90209774+crackededed@users.noreply.github.com>
2023-02-02 11:21:50 +0200

    bttv webp
    (cherry picked from commit f6254541b90a2cef65c07c316464cc2e17fdaeb5)

--- app/src/main/java/com/github/andreyasadchy/xtra/model/chat/TwitchEmote.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/model/chat/TwitchEmote.kt
@@ -6,7 +6,7 @@ class TwitchEmote(
         override val url2x: String? = "https://static-cdn.jtvnw.net/emoticons/v2/$name/default/dark/2.0",
         override val url3x: String? = "https://static-cdn.jtvnw.net/emoticons/v2/$name/default/dark/2.0",
         override val url4x: String? = "https://static-cdn.jtvnw.net/emoticons/v2/$name/default/dark/3.0",
-        override val type: String? = "image/gif",
+        override val type: String? = "gif",
         override val isZeroWidth: Boolean = false,
         var begin: Int = 0,
         var end: Int = 0,
