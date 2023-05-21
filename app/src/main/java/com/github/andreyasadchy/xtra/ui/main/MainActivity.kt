46f33bd94bf3af93cdb9a9a3ac2b2e2a3db8133c
crackededed <90209774+crackededed@users.noreply.github.com>
2023-05-21 20:30:13 +0300

    fix pip check
    (cherry picked from commit 84967d15273ec26ba3bfaba76150e772807d82d6)

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/main/MainActivity.kt
@@ -200,7 +200,7 @@ class MainActivity : AppCompatActivity(), SlidingLayout.Listener {
 
     override fun onUserLeaveHint() {
         super.onUserLeaveHint()
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE) &&
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE) && Build.VERSION.SDK_INT < Build.VERSION_CODES.S && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O &&
             prefs.getString(C.PLAYER_BACKGROUND_PLAYBACK, "0") == "0" && playerFragment?.enterPictureInPicture() == true) {
             try {
                 enterPictureInPictureMode(PictureInPictureParams.Builder().build())
@@ -333,7 +333,7 @@ class MainActivity : AppCompatActivity(), SlidingLayout.Listener {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.playerContainer, fragment).commit()
         viewModel.onPlayerStarted()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && prefs.getString(C.PLAYER_BACKGROUND_PLAYBACK, "0") == "0") {
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && prefs.getString(C.PLAYER_BACKGROUND_PLAYBACK, "0") == "0") {
             setPictureInPictureParams(PictureInPictureParams.Builder().setAutoEnterEnabled(true).build())
         }
     }
@@ -345,7 +345,7 @@ class MainActivity : AppCompatActivity(), SlidingLayout.Listener {
                 .commit()
         playerFragment = null
         viewModel.onPlayerClosed()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             setPictureInPictureParams(PictureInPictureParams.Builder().setAutoEnterEnabled(false).build())
         }
     }
