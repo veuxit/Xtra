683853a55ac38ac0c5039e5919a0ca6f0d21caea
ttmx <git@tteles.dev>
2023-05-23 15:49:05 +0100

    Fix image overlapping
    (cherry picked from commit 921a05f90cb6bd0d8c3a64bb1b9630d2ca6ae388)

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/view/chat/CenteredImageSpan.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/view/chat/CenteredImageSpan.kt
@@ -7,56 +7,47 @@ import android.graphics.drawable.Drawable
 import android.text.style.ImageSpan
 import java.lang.ref.WeakReference
 
-class CenteredImageSpan( drawable: Drawable) : ImageSpan(
-     drawable
+
+class CenteredImageSpan(drawable: Drawable) : ImageSpan(
+    drawable
 ) {
     private var mDrawableRef: WeakReference<Drawable?>? = null
     override fun getSize(
-        paint: Paint,
-        text: CharSequence,
-        start: Int,
-        end: Int,
-        fm: FontMetricsInt?
+        paint: Paint, text: CharSequence?, start: Int, end: Int,
+        fontMetricsInt: FontMetricsInt?
     ): Int {
-        val d = cachedDrawable
-        val rect = d!!.bounds
-        if (fm != null) {
-            val pfm = paint.fontMetricsInt
-            // keep it the same as paint's fm
-            fm.ascent = pfm.ascent
-            fm.descent = pfm.descent
-            fm.top = pfm.top
-            fm.bottom = pfm.bottom
+        val drawable = drawable
+        val rect = drawable.bounds
+        if (fontMetricsInt != null) {
+            val fmPaint = paint.fontMetricsInt
+            val fontHeight = fmPaint.descent - fmPaint.ascent
+            val drHeight = rect.bottom - rect.top
+            val centerY = fmPaint.ascent + fontHeight / 2
+            fontMetricsInt.ascent = centerY - drHeight / 2
+            fontMetricsInt.top = fontMetricsInt.ascent
+            fontMetricsInt.bottom = centerY + drHeight / 2
+            fontMetricsInt.descent = fontMetricsInt.bottom
         }
         return rect.right
     }
-
     override fun draw(
-        canvas: Canvas,
-        text: CharSequence,
-        start: Int,
-        end: Int,
-        x: Float,
-        top: Int,
-        y: Int,
-        bottom: Int,
-        paint: Paint
+        canvas: Canvas, text: CharSequence?, start: Int, end: Int,
+        x: Float, top: Int, y: Int, bottom: Int, paint: Paint
     ) {
-        val b = cachedDrawable
+        val drawable = drawable
         canvas.save()
-        val drawableHeight = b!!.intrinsicHeight
-        val fontAscent = paint.fontMetricsInt.ascent
-        val fontDescent = paint.fontMetricsInt.descent
-        val transY = bottom - b.bounds.bottom +  // align bottom to bottom
-                (drawableHeight - fontDescent + fontAscent) / 2 // align center to center
+        val fmPaint = paint.fontMetricsInt
+        val fontHeight = fmPaint.descent - fmPaint.ascent
+        val centerY = y + fmPaint.descent - fontHeight / 2
+        val transY = centerY - (drawable.bounds.bottom - drawable.bounds.top) / 2
         canvas.translate(x, transY.toFloat())
-        b.draw(canvas)
+        drawable.draw(canvas)
         canvas.restore()
     }
 
     // Redefined locally because it is a private member from DynamicDrawableSpan
     private val cachedDrawable: Drawable?
-        private get() {
+        get() {
             val wr = mDrawableRef
             var d: Drawable? = null
             if (wr != null) d = wr.get()
