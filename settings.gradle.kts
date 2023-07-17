f1d12081c007c56940c4534cfc38875cbe2570f7
crackededed <90209774+crackededed@users.noreply.github.com>
2023-07-17 20:54:22 +0300

    kotlin dsl

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,8 +10,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
         jcenter()
     }
 }
 rootProject.name = "Xtra"
-include ':app'
+include(":app")
