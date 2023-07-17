c8c8611e42691e5daaac97fa90eda346c63a12cf
crackededed <90209774+crackededed@users.noreply.github.com>
2023-07-17 20:53:19 +0300

    kotlin dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,142 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.navigation.safeargs)
+    alias(libs.plugins.apollo3)
+}
+
+android {
+    signingConfigs {
+        getByName("debug") {
+            keyAlias = "debug"
+            keyPassword = "123456"
+            storeFile = file("debug-keystore.jks")
+            storePassword = "123456"
+        }
+    }
+    namespace = "com.github.andreyasadchy.xtra"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.github.andreyasadchy.xtra"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 121
+        versionName = "2.22.1"
+        resourceConfigurations += listOf("ar", "de", "en", "es", "fr", "in", "ja", "pt-rBR", "ru", "tr")
+    }
+
+    buildTypes {
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-DEBUG"
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        getByName("release") {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+    lint {
+        disable += "ContentDescription"
+    }
+}
+
+dependencies {
+    //UI
+    implementation(libs.material)
+    implementation(libs.appcompat)
+    implementation(libs.constraintlayout)
+    implementation(libs.preference.ktx)
+    implementation(libs.swiperefreshlayout)
+    implementation(libs.flexbox)
+    implementation(libs.draglistview)
+
+    //Architecture components
+    implementation(libs.paging.runtime.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.common.java8)
+    implementation(libs.lifecycle.process)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    kapt(libs.room.compiler)
+    implementation(libs.core.ktx)
+    implementation(libs.fragment.ktx)
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
+    implementation(libs.webkit)
+
+    //Misc
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp.logging.interceptor)
+
+    implementation(libs.apollo.runtime)
+
+    implementation(libs.request.inspector.webview)
+
+    implementation(libs.fetch)
+    implementation(libs.fetch.okhttp)
+    implementation(libs.open.m3u8)
+
+    implementation(libs.media3.exoplayer)
+    implementation(libs.media3.exoplayer.hls)
+    implementation(libs.media3.datasource.okhttp)
+    implementation(libs.media3.ui)
+    implementation(libs.media3.session)
+    implementation(libs.checker.qual)
+
+    implementation(libs.coil)
+    implementation(libs.coil.gif)
+
+    implementation(libs.glide)
+    kapt(libs.glide.compiler)
+    implementation(libs.glide.okhttp)
+    implementation(libs.glide.webpdecoder)
+
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
+
+    implementation(libs.coroutines)
+}
+
+kapt {
+    arguments {
+        arg("room.schemaLocation", "$projectDir/schemas")
+    }
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.github.andreyasadchy.xtra")
+        outputDirConnection {
+            connectToAndroidSourceSet("main")
+        }
+    }
+}
+
+// Delete large build log files from ~/.gradle/daemon/X.X/daemon-XXX.out.log
+// Source: https://discuss.gradle.org/t/gradle-daemon-produces-a-lot-of-logs/9905
+File("${project.gradle.gradleUserHomeDir.absolutePath}/daemon/${project.gradle.gradleVersion}").listFiles()?.forEach {
+    if (it.name.endsWith(".out.log")) {
+        // println("Deleting gradle log file: $it") // Optional debug output
+        it.delete()
+    }
+}
