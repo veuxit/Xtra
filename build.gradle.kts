e61e31ebe6ed48dc17c3ad8822b6e66f55672f8f
crackededed <90209774+crackededed@users.noreply.github.com>
2023-11-08 15:35:27 +0200

    ksp
    (cherry picked from commit 2bc269c1bb9e0af6a14d919d7c964f47a2d041ca)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
