a949dffce9878d4eb9086f634170602412cb177f
crackededed <90209774+crackededed@users.noreply.github.com>
2023-11-08 15:34:55 +0200

    refactor
    (cherry picked from commit 7cc65ad8b7ea0c105f07bceab93e68d8863a8912)

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/settings/SettingsActivity.kt
@@ -6,24 +6,38 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
+import androidx.core.util.Pair
+import androidx.core.widget.NestedScrollView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SeekBarPreference
 import androidx.preference.SwitchPreferenceCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.andreyasadchy.xtra.R
 import com.github.andreyasadchy.xtra.databinding.ActivitySettingsBinding
 import com.github.andreyasadchy.xtra.ui.Utils
 import com.github.andreyasadchy.xtra.ui.main.IntegrityDialog
-import com.github.andreyasadchy.xtra.ui.settings.api.DragListFragment
 import com.github.andreyasadchy.xtra.util.C
 import com.github.andreyasadchy.xtra.util.DisplayUtils
+import com.github.andreyasadchy.xtra.util.TwitchApiHelper
 import com.github.andreyasadchy.xtra.util.applyTheme
+import com.github.andreyasadchy.xtra.util.convertDpToPixels
 import com.github.andreyasadchy.xtra.util.prefs
 import com.github.andreyasadchy.xtra.util.shortToast
+import com.woxthebox.draglistview.DragItemAdapter
+import com.woxthebox.draglistview.DragListView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -191,7 +205,7 @@ class SettingsActivity : AppCompatActivity() {
             findPreference<Preference>("api_settings")?.setOnPreferenceClickListener {
                 parentFragmentManager
                     .beginTransaction()
-                    .replace(R.id.settings, DragListFragment.newInstance())
+                    .replace(R.id.settings, DragListFragment())
                     .addToBackStack(null)
                     .commit()
                 true
@@ -319,6 +333,110 @@ class SettingsActivity : AppCompatActivity() {
     class TokenSettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.token_preferences, rootKey)
+        }
+    }
+
+    class DragListFragment : Fragment() {
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            var newId = 1000
+            val view = LinearLayout(requireContext()).apply {
+                orientation = LinearLayout.VERTICAL
+                addView(FrameLayout(requireContext()).apply {
+                    id = newId
+                })
+            }
+            childFragmentManager.beginTransaction().replace(newId, ApiSettingsFragment()).commit()
+            mapOf(
+                getString(R.string.games) to Pair(C.API_PREF_GAMES, TwitchApiHelper.gamesApiDefaults),
+                getString(R.string.streams) to Pair(C.API_PREF_STREAMS, TwitchApiHelper.streamsApiDefaults),
+                getString(R.string.game_streams) to Pair(C.API_PREF_GAME_STREAMS, TwitchApiHelper.gameStreamsApiDefaults),
+                getString(R.string.game_videos) to Pair(C.API_PREF_GAME_VIDEOS, TwitchApiHelper.gameVideosApiDefaults),
+                getString(R.string.game_clips) to Pair(C.API_PREF_GAME_CLIPS, TwitchApiHelper.gameClipsApiDefaults),
+                getString(R.string.channel_videos) to Pair(C.API_PREF_CHANNEL_VIDEOS, TwitchApiHelper.channelVideosApiDefaults),
+                getString(R.string.channel_clips) to Pair(C.API_PREF_CHANNEL_CLIPS, TwitchApiHelper.channelClipsApiDefaults),
+                getString(R.string.search_videos) to Pair(C.API_PREF_SEARCH_VIDEOS, TwitchApiHelper.searchVideosApiDefaults),
+                getString(R.string.search_streams) to Pair(C.API_PREF_SEARCH_STREAMS, TwitchApiHelper.searchStreamsApiDefaults),
+                getString(R.string.search_channels) to Pair(C.API_PREF_SEARCH_CHANNEL, TwitchApiHelper.searchChannelsApiDefaults),
+                getString(R.string.search_games) to Pair(C.API_PREF_SEARCH_GAMES, TwitchApiHelper.searchGamesApiDefaults),
+                getString(R.string.followed_streams) to Pair(C.API_PREF_FOLLOWED_STREAMS, TwitchApiHelper.followedStreamsApiDefaults),
+                getString(R.string.followed_videos) to Pair(C.API_PREF_FOLLOWED_VIDEOS, TwitchApiHelper.followedVideosApiDefaults),
+                getString(R.string.followed_channels) to Pair(C.API_PREF_FOLLOWED_CHANNELS, TwitchApiHelper.followedChannelsApiDefaults),
+                getString(R.string.followed_games) to Pair(C.API_PREF_FOLLOWED_GAMES, TwitchApiHelper.followedGamesApiDefaults),
+            ).forEach { entry ->
+                newId++
+                view.addView(TextView(requireContext()).apply {
+                    text = entry.key
+                    layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT).apply {
+                        setMargins(context.convertDpToPixels(10F), context.convertDpToPixels(3F), 0, context.convertDpToPixels(3F))
+                    }
+                    setTextSize(TypedValue.COMPLEX_UNIT_SP, 16F)
+                })
+                val list = TwitchApiHelper.listFromPrefs(requireActivity().prefs().getString(entry.value.first, ""), entry.value.second)
+                view.addView((inflater.inflate(R.layout.drag_list_layout, container, false) as DragListView).apply {
+                    id = newId
+                    setLayoutManager(LinearLayoutManager(context))
+                    setAdapter(DragListAdapter(list), true)
+                    setCanDragHorizontally(false)
+                    setCanDragVertically(true)
+                    setDragListListener(object : DragListView.DragListListenerAdapter() {
+                        override fun onItemDragStarted(position: Int) {}
+
+                        override fun onItemDragEnded(fromPosition: Int, toPosition: Int) {
+                            if (fromPosition != toPosition) {
+                                var str = ""
+                                adapter.itemList.forEachIndexed { index, item ->
+                                    str = "$str${index.toLong()}:${(item as Pair<Long, String>).second},"
+                                }
+                                requireActivity().prefs().edit { putString(entry.value.first, str) }
+                            }
+                        }
+                    })
+                })
+            }
+            return NestedScrollView(requireContext()).apply { addView(view) }
+        }
+
+        class DragListAdapter(list: ArrayList<Pair<Long?, String?>?>?) : DragItemAdapter<Pair<Long?, String?>?, DragListAdapter.ViewHolder>() {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.drag_list_item, parent, false)
+                return ViewHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+                super.onBindViewHolder(holder, position)
+                holder.mText.text = mItemList[position]!!.second!!
+            }
+
+            override fun getUniqueItemId(position: Int): Long = mItemList[position]!!.first!!
+
+            class ViewHolder(itemView: View) : DragItemAdapter.ViewHolder(itemView, R.id.image, false) {
+                val mText: TextView = itemView.findViewById(R.id.text)
+            }
+
+            init {
+                itemList = list
+            }
+        }
+    }
+
+    class ApiSettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.api_preferences, rootKey)
+
+            findPreference<Preference>("api_token_settings")?.setOnPreferenceClickListener {
+                requireParentFragment().parentFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.settings, ApiTokenSettingsFragment())
+                    .addToBackStack(null)
+                    .commit()
+                true
+            }
+        }
+    }
+
+    class ApiTokenSettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.api_token_preferences, rootKey)
         }
     }
 }
