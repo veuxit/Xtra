806a75c4a516ce71d5a0e2e43b3f27c200d44894
crackededed <90209774+crackededed@users.noreply.github.com>
2023-11-18 18:47:49 +0200

    stream request headers
    (cherry picked from commit 73b166e8accc6b89716706143caf4265c5edac50)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 16
         targetSdk = 34
         versionCode = 121
-        versionName = "2.26.1"
+        versionName = "2.26.2"
         resourceConfigurations += listOf("ar", "de", "en", "es", "fr", "in", "ja", "pt-rBR", "ru", "tr", "zh-rTW")
     }
 
