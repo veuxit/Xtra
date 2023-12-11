63f9f233586a857208b0a835c63081d380dc8cd8
crackededed <90209774+crackededed@users.noreply.github.com>
2023-12-11 16:45:56 +0200

    fix autocomplete duplicates
    (cherry picked from commit 8711e866fb775a6ee5d7369d00f19558c965c156)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 16
         targetSdk = 34
         versionCode = 121
-        versionName = "2.26.3"
+        versionName = "2.26.4"
         resourceConfigurations += listOf("ar", "de", "en", "es", "fr", "in", "ja", "pt-rBR", "ru", "tr", "zh-rTW")
     }
 
