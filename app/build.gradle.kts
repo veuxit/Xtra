47372caf2ae03c42e375eeeb60361f7bceb53d50
crackededed <90209774+crackededed@users.noreply.github.com>
2024-01-25 21:37:20 +0200

    fix monochrome icon
    (cherry picked from commit 917e17f487d8288ef4ffb6f26933288081c4e41e)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.github.andreyasadchy.xtra"
         minSdk = 21
         targetSdk = 34
-        versionCode = 221
-        versionName = "2.27.3"
+        versionCode = 222
+        versionName = "2.27.4"
         resourceConfigurations += listOf("ar", "de", "en", "es", "fr", "in", "ja", "pt-rBR", "ru", "tr", "zh-rTW")
     }
 
