27df62dc45c31df1752e321365d278859af7e7bb
crackededed <90209774+crackededed@users.noreply.github.com>
2024-02-03 20:11:29 +0200

    updated queries
    (cherry picked from commit 91ac65f1854bb8357b98e90d395d8bb9f19f18fa)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.github.andreyasadchy.xtra"
         minSdk = 21
         targetSdk = 34
-        versionCode = 223
-        versionName = "2.28.0"
+        versionCode = 224
+        versionName = "2.29.0"
         resourceConfigurations += listOf("ar", "de", "en", "es", "fr", "in", "ja", "pt-rBR", "ru", "tr", "zh-rTW", "zh-rCN")
     }
 
