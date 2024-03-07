a580fafea4b300c445e671f23cd3af5d4535b78f
crackededed <90209774+crackededed@users.noreply.github.com>
2024-03-07 19:04:32 +0200

    fix timestamp offset

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 121
-        versionName = "2.30.0"
+        versionName = "2.30.1"
         resourceConfigurations += listOf("ar", "de", "en", "es", "fr", "in", "ja", "pt-rBR", "ru", "tr", "zh-rTW", "zh-rCN")
     }
 
