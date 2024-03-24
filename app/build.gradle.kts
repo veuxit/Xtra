d048c93a068db8dcb4867caf8792ea104cb855d2
crackededed <90209774+crackededed@users.noreply.github.com>
2024-03-24 16:34:55 +0200

    notification icon
    (cherry picked from commit d15b83a8dd95d08842a84b16326d11723d3f3d2d)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 16
         targetSdk = 34
         versionCode = 121
-        versionName = "2.30.4"
+        versionName = "2.30.5"
         resourceConfigurations += listOf("ar", "de", "en", "es", "fr", "in", "it", "ja", "pt-rBR", "ru", "tr", "zh-rTW", "zh-rCN")
     }
 
