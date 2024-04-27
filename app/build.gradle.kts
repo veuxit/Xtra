763d1e58e555b5f29f5540906d7fba5da4cc5a29
crackededed <90209774+crackededed@users.noreply.github.com>
2024-04-27 21:12:28 +0300

    channel points fix
    (cherry picked from commit 87b8677c59ccdc0d76fa5f8ee2aa516176596884)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 16
         targetSdk = 34
         versionCode = 121
-        versionName = "2.31.1"
+        versionName = "2.31.2"
         resourceConfigurations += listOf("ar", "de", "en", "es", "fr", "in", "it", "ja", "pt-rBR", "ru", "tr", "zh-rTW", "zh-rCN")
     }
 
