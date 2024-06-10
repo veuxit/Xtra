dda35b3c6d6924189c06fd0a67d377573a7de856
crackededed <90209774+crackededed@users.noreply.github.com>
2024-06-10 14:55:07 +0300

    fix database migration
    (cherry picked from commit d84601925546baa9a9155a547f572d6fa35cd770)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.github.andreyasadchy.xtra"
         minSdk = 21
         targetSdk = 34
-        versionCode = 241
-        versionName = "2.32.2"
+        versionCode = 242
+        versionName = "2.32.3"
     }
 
     buildTypes {
