9b2dd061f0aa60fe23bd617b7eaa12b075ca53fc
crackededed <90209774+crackededed@users.noreply.github.com>
2025-05-17 13:07:40 +0300

    avoid rounded screen corners

--- app/src/main/java/com/github/andreyasadchy/xtra/util/C.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/util/C.kt
@@ -153,6 +153,7 @@ object C {
     const val PLAYER_KEEP_SCREEN_ON_WHEN_PAUSED = "player_keep_screen_on_when_paused"
     const val PLAYER_AUDIO_FOCUS = "player_audio_focus"
     const val PLAYER_HANDLE_AUDIO_BECOMING_NOISY = "player_handle_audio_becoming_noisy"
+    const val PLAYER_ROUNDED_CORNER_PADDING = "player_rounded_corner_padding"
     const val PLAYER_KEEP_CHAT_OPEN = "player_keep_chat_open"
     const val PLAYER_HIDE_ADS = "player_hide_ads"
     const val PLAYER_PROXY = "player_proxy"
