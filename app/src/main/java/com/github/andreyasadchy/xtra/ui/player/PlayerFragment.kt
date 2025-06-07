933092c24c74db9e3ba44fa940fbd8852e90a805
crackededed <90209774+crackededed@users.noreply.github.com>
2025-06-07 16:42:10 +0300

    fix crash

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/player/PlayerFragment.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/player/PlayerFragment.kt
@@ -898,7 +898,7 @@ class PlayerFragment : BaseNetworkFragment(), SlidingLayout.Listener, PlayerGame
                 resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
             } else {
                 requireActivity().window.decorView.setOnSystemUiVisibilityChangeListener {
-                    if (!isKeyboardShown && slidingLayout.isMaximized) {
+                    if (!isKeyboardShown && slidingLayout.isMaximized && activity != null) {
                         hideStatusBar()
                     }
                 }
