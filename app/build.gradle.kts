b87bdaf6460fdca54ddac388629c274060ee59f0
crackededed <90209774+crackededed@users.noreply.github.com>
2025-11-04 11:33:23 +0200

    fix debug settings crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 23
         targetSdk = 36
         versionCode = 121
-        versionName = "2.51.0"
+        versionName = "2.51.0.1"
     }
 
     buildTypes {
@@ -67,7 +67,7 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.gms:play-services-cronet:18.1.1")
+    implementation("com.google.android.gms:play-services-cronet:18.1.0")
     implementation("com.google.mlkit:language-id:17.0.6")
     implementation("com.google.mlkit:translate:17.0.3")
 
